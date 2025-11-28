c9c244a8e9f990b0ea090ee80c98df81475f6ac9
curiosAngularBoy <barabasi.alex@gmail.com>
2025-11-28 15:16:33 +0200

    Minimal Warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-  println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+  let root = warp::path::end()
+    .map(|| "Hello, world!");
+
+  let hello = warp::path!("hello" / String)
+    .map(|name: String| format!("Hello, {}!", name));
+
+  let routes = root.or(hello);
+
+  warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
