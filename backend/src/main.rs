b1268444f36fe9a96541fb166055cff9e3b54359
Kovács György <georgesmth202@gmail.com>
2025-12-17 14:47:28 +0200

    implemented get and insert for articles (#6)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,16 +6,10 @@ use std::sync::{Arc, Mutex};
 use rusqlite::Connection;
 use warp::Filter;
 
-use crate::article::Article;
-
 #[tokio::main]
 async fn main() {
     let connection = db::init();
-
-    db::insert_article(Article::default(), &connection).unwrap();
-
     let connection: Arc<Mutex<Connection>> = Arc::new(Mutex::new(connection));
-
     let connection_filter = warp::any().map(move || connection.clone());
 
     let root = warp::path::end().map(handlers::serve_webpage);
@@ -23,6 +17,8 @@ async fn main() {
     let save_article =
         warp::post()
         .and(warp::path!("api" / "article"))
+        .and(warp::body::json())
+        .and(connection_filter.clone())
         .map(handlers::save_article);
 
     let get_articles = warp::path!("articles")
