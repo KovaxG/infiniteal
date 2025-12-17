62dac0fd97b9222d26000abc6c039ae536a13b46
KovaxG <georgesmth202@gmail.com>
2025-12-17 13:05:57 +0200

    extracted handlers to new file

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,12 @@
+mod handlers;
 mod article;
 mod db;
 
-use crate::article::Article;
-use bytes::Buf;
-use futures::{StreamExt, TryStreamExt};
-use rusqlite::{Connection, Result};
-use std::fs;
 use std::sync::{Arc, Mutex};
-use tokio::io::AsyncWriteExt;
-use warp::reply::{html, json};
-use warp::{Filter, Rejection, Reply};
+use rusqlite::Connection;
+use warp::Filter;
 
-async fn get_all_articles(connection: Arc<Mutex<Connection>>) -> Result<impl Reply, Rejection> {
-    let connection = connection.lock().unwrap();
-    let response = db::get_all_articles(&connection).unwrap();
-    Ok(json(&response))
-}
+use crate::article::Article;
 
 #[tokio::main]
 async fn main() {
@@ -27,63 +18,23 @@ async fn main() {
 
     let connection_filter = warp::any().map(move || connection.clone());
 
-    let root = warp::path::end().map(|| {
-        let contents = if fs::exists("index.html").unwrap() {
-            fs::read_to_string("index.html").unwrap()
-        } else {
-            "Go to frontend folder and run sh compile.sh to get an index.html".to_string()
-        };
+    let root = warp::path::end().map(handlers::serve_webpage);
 
-        html(contents)
-    });
-
-    let upload = warp::post()
-        .and(warp::path("upload"))
-        .and(warp::multipart::form().max_length(10_000_000)) // limit size (5MB)
-        .and_then(handle_upload);
-
-    let hello = warp::path!("hello" / String).map(|name: String| format!("Hello, {}!", name));
+    let save_article =
+        warp::post()
+        .and(warp::path!("api" / "article"))
+        .map(handlers::save_article);
 
     let get_articles = warp::path!("articles")
         .and(connection_filter.clone())
-        .and_then(get_all_articles);
+        .and_then(handlers::get_all_articles);
 
-    let routes = root.or(hello).or(get_articles).or(upload);
+    let upload = warp::post()
+        .and(warp::path("upload"))
+        .and(warp::multipart::form().max_length(10_000_000))
+        .and_then(handlers::handle_upload);
+
+    let routes = root.or(save_article).or(get_articles).or(upload);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
-}
-
-async fn handle_upload(mut form: warp::multipart::FormData) -> Result<impl Reply, Rejection> {
-    while let Some(part) = form.try_next().await.map_err(|_| warp::reject())? {
-        println!("Part name     : {}", part.name());
-        let name = part.name().to_string();
-
-        if let Some(filename) = part.filename() {
-            println!("Part filename : {}", filename);
-        } else {
-            println!("Part has no filename");
-        }
-        let filename = part.filename().map(str::to_owned);
-
-        println!("Part content-type: {:?}", part.content_type());
-
-        if name == "file" {
-            let filename = filename.unwrap_or_else(|| "upload.pdf".to_string());
-            let mut file = tokio::fs::File::create(&filename)
-                .await
-                .map_err(|_| warp::reject())?;
-
-            let mut stream = part.stream();
-
-            while let Some(chunk) = stream.next().await {
-                let chunk = chunk.map_err(|_| warp::reject())?;
-                file.write_all(chunk.chunk())
-                    .await
-                    .map_err(|_| warp::reject())?;
-            }
-
-            file.flush().await.map_err(|_| warp::reject())?;
-        }
-    }
-    Ok("ok")
 }
